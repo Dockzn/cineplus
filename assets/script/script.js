a86aeed1bfec28ac504737bc96b6c32ed474ce53
Diego Amorim - Dockzn <diego91077@gmail.com>
2024-07-28 21:06:41 -0300

    Adição de arquivos v1.01

---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+const apiKey =  'eb519aeb'
+const frmPesquisa = document.querySelector("form")
+
+frmPesquisa.onsubmit = (ev) =>{
+    ev.preventDefault()
+
+    const pesquisa = ev.target.pesquisa.value
+
+    if(pesquisa == ""){
+        alert("Preencha o campo nescessario!")
+        return
+    }
+    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apiKey}`)
+    .then(result => result.json())
+    .then(json => carregaLista(json))
+}
+
+    const carregaLista = (json) => {
+        const lista = document.querySelector('div.lista')
+        lista.innerHTML = ''
+
+        if (json.Response == "False"){
+            alert("Nenhum filme encontrado")
+            return
+        } 
+
+        json.Search.forEach(element => {
+
+            let item = document.createElement("div")
+            item.classList.add("item")
+
+            item.innerHTML =`<img src="${element.Poster}" /><h2>${element.Title}</h2>`
+
+            lista.appendChild(item);
+        })
+}
